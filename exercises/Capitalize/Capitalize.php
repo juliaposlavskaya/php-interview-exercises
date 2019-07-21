665529960b23015dc3388bf60ba5bf603f03797a
JuliaPoslavskaya <julia.poslavskayaya@gmail.com>
2019-07-21 16:07:00 +0300

    Capitalize done

--- exercises/Capitalize/Capitalize.php
+++ exercises/Capitalize/Capitalize.php
@@ -7,10 +7,26 @@ namespace Exercises\Capitalize;
 /**
  * Capitalize each word.
  *
- * @method static string get(string $string)
+// * @method static string get(string $string)
  * @example Capitalize::get('hello there') === 'Hello There'
  * @example Capitalize::get("hey, so it's working!") === "Hey, So It's Working!"
  */
 final class Capitalize
 {
+    /**
+     * @param string $string
+     *
+     * @return string
+     */
+    public static function get(string $string): string
+    {
+        $result = [];
+        $worlds = explode(' ', $string);
+
+        foreach ($worlds as $world) {
+            $result[] = ucfirst($world);
+        }
+
+        return implode(' ', $result);
+    }
 }
