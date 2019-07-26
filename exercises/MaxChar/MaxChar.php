f39bea3c7cbb492bcb8edecce9fdd90dab0ca7e0
JuliaPoslavskaya <julia.poslavskayaya@gmail.com>
2019-07-27 00:22:39 +0300

    MaxChar done

--- exercises/MaxChar/MaxChar.php
+++ exercises/MaxChar/MaxChar.php
@@ -7,10 +7,30 @@ namespace Exercises\MaxChar;
 /**
  * Find the most used character.
  *
- * @method static string get(string $string)
+ //* @method static string get(string $string)
  * @example MaxChar::get('qqweqrty')  === 'q'
  * @example MaxChar::get('apple 2202')  === '2'
  */
 final class MaxChar
 {
+    /**
+     * @param string $string
+     *
+     * @return string
+     */
+    public static function get(string $string): string
+    {
+        $array = str_split($string);
+        $count = 0;
+        $result = '';
+        foreach ($array as $value) {
+            $newCount = substr_count($string, $value);
+            if ($newCount > $count) {
+                $count = $newCount;
+                $result = $value;
+            }
+        }
+
+        return $result;
+    }
 }
