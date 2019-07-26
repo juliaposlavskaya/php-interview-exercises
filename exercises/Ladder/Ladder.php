6f16543210ac62e4120785a9d6e89ade0c0ebb98
JuliaPoslavskaya <julia.poslavskayaya@gmail.com>
2019-07-27 00:05:45 +0300

    Ladder done

--- exercises/Ladder/Ladder.php
+++ exercises/Ladder/Ladder.php
@@ -7,11 +7,28 @@ namespace Exercises\Ladder;
 /**
  * Print a ladder.
  *
- * @method static void print(int $level)
+ //* @method static void print(int $level)
  * @example Ladder::print(3)  -> '#  '
  *                               '## '
  *                               '###'
  */
 final class Ladder
 {
+    /**
+     * @param int $level
+     */
+    public static function print(int $level): void
+    {
+        for ($row = 0; $row < $level; $row++) {
+            $string  = '';
+            for ($column = 0; $column < $level; $column++) {
+                $string .= $column >= $row ? '#' : ' ';
+            }
+            if ($row == 0) {
+                echo $string;
+            } else {
+                echo $string, PHP_EOL;
+            }
+        }
+    }
 }
